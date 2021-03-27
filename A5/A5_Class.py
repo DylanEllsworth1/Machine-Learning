3c836c7314d9df84cda4f70fea32f07186555044
oscarzqy@hotmail.com <oscarzqy@hotmail.com>
2021-03-27 23:47:26 +0800

    fix missing imports

--- A5/A5_Class.py
+++ A5/A5_Class.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.model_selection import learning_curve
 from sklearn import metrics
+from sklearn.model_selection import KFold
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
